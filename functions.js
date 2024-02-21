e209dbb0687fcbc495779c0f1073931d6d2f8848
siddhardha-reddy <siddhardhalella@gmail.com>
2024-02-21 11:35:25 +0530

    ntg

---
+++ functions.js
@@ -0,0 +1,39 @@
+// function hello(a,b){
+//     c=a+b;
+//     alert(c);
+// }
+// hello("siddhardha ","Reddy");
+
+
+// var add=function sumNumbers(a,b,c){
+//     d=a+b+c;
+//     console.log(d);
+//     return d;
+// }
+// // sumNumbers(1,2,3);   we will get an error sumNumbers is not defined
+// var sum=add(1,2,3);
+// console.log(add(10,20,30));
+// alert(sum);
+// // arrays:- these are used to store the date of same data type
+// // if we try to store data of diff data type it will become as object 
+// // functions are of fundamental bulding blocks these are simillar to procedures
+// // we can return values through it after there will be some relation b/w returned value and internal statements in the function
+// // function square(num){
+// // return num*num;
+// // }
+
+// function addd(){
+//     return 45;
+// } 
+function ClickTheButton(){
+    alert("During onclick action is triggered");
+}
+function textChange(){
+    console.log("you are entering the text  ")
+}
+function movingCursor(){
+    console.log("moving out of text");
+}
+function onCursor(){
+    alert("data hovered");
+}
